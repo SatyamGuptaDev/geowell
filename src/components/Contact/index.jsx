052bdb42ea658bc2faac6ba71446248a1a168bdf
Satyam <90516085+SatyamGuptaDev@users.noreply.github.com>
2023-11-30 04:10:56 +0000

    done...

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -22,13 +22,12 @@ function Contact() {
         <h1 className="heading" data-aos="zoom-out">Meet the <span className="span text-transparent bg-clip-text ">Team</span> </h1>
           <div className='contact-container' data-aos="fade-in" data-aos-delay="1000">
               <Card name="Satyam Gupta" title="Front-End Developer" image="https://i.postimg.cc/cJtN25jq/Whats-App-Image-2023-09-17-at-23-53-53.jpg"/>  
-            <Card name="Tanishq Agarwal" title="Back-End Developer" image="https://media.licdn.com/dms/image/D4D03AQErSFQvvNkzmA/profile-displayphoto-shrink_800_800/0/1691864426120?e=1701302400&v=beta&t=lnPeW4M6mbORNwgcYZXLW1ABT9y6oaklbUCmaKhPmxc" />
-              <Card name="Ayushi Dwivedi" title="ML Developer" image="https://media.licdn.com/dms/image/D4D03AQHVTV78NLknYg/profile-displayphoto-shrink_800_800/0/1692426271520?e=1701302400&v=beta&t=I7XJI1KBGO4B9MWzvtKTvdWkF4cEI3VXSQDJtKdQcaw"/>
+              <Card name="Satyam Singh" title="Front-End Developer" image="https://i.postimg.cc/T27vMRVj/singh.jpg"/>
+              <Card name="Satvik Trivedi" title="ML Developer" image="https://i.postimg.cc/QdpKFWCr/IMG20231130005908.jpg"/>
           </div>
           <div className='contact-container' data-aos="fade-in" data-aos-delay="2000">
-              <Card name="Satyam Singh" title="Web Developer" image="https://media.licdn.com/dms/image/D4D03AQHjyLJxN6LLZw/profile-displayphoto-shrink_800_800/0/1686240990025?e=1701302400&v=beta&t=sTTrISVOArn3QGzr_SwURpInpKOvMOTQcLgv7LqrSxk"/>
-              <Card name="Aditya Sachan" title="Flutter Developer"  image="https://media.licdn.com/dms/image/D4D03AQHp8s78Z3z9sA/profile-displayphoto-shrink_800_800/0/1688918247577?e=1701302400&v=beta&t=2WBSZW8DKUW71FmGrIC5lbhns1HK1jySJ4HSSi5UpGw"/>
-              <Card name="Avni Jain" title="ML Developer" image="https://media.licdn.com/dms/image/C4D03AQHHLpk1-jpxXg/profile-displayphoto-shrink_800_800/0/1654345583174?e=1701302400&v=beta&t=c5LbGJVI0lFClKrnGks_b3seoO4o-pkC2mhZL7i_ufI"/>
+          <Card name="Shreeyansh Aditya " title="Back-End Developer" image="https://i.postimg.cc/FRSktPq3/shreeyansh.jpg" />
+ 
           </div>
       </div>
 
